e87d68a07e26da8e153866c4189eefc561a7a578
wakkpu <ho0524870@gmail.com>
2024-03-09 21:48:14 +0900

    [INIT] 예제 프로젝트 생성

---
+++ src/main/java/hello/advanced/app/v0/OrderController.java
@@ -0,0 +1,17 @@
+package hello.advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+
+    @GetMapping("/v0/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+}
