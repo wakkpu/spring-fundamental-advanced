1b907e0719a951e6842eeab059dc856b95d659c6
wakkpu <ho0524870@gmail.com>
2024-03-09 21:56:55 +0900

    [INIT] 예제 프로젝트 생성

--- src/main/java/hello/advanced/AdvancedApplication.java
+++ src/main/java/hello/advanced/AdvancedApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AdvancedApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AdvancedApplication.class, args);
 	}
