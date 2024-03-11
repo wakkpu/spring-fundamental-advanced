bab191efae36339ce4fc1c3f41d41d783b9c5e4a
wakkpu <ho0524870@gmail.com>
2024-03-11 23:28:23 +0900

    [FEAT] 스레드 로컬을 통한 traceId 동기화 - 메서드에 파라미터로 traceId를 매번 넘겨줄 필요가 없어졌다 - 하지만 동시성 이슈가 남아있다   - A 트랜잭션이 동작 중일 때 B 트랜잭션이 끼어들면?

---
+++ src/test/java/hello/advanced/trace/threadlocal/FieldServiceTest.java
@@ -0,0 +1,45 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.FieldService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class FieldServiceTest {
+    private FieldService fieldService = new FieldService();
+
+    @Test
+    void field() {
+        log.info("main start");
+
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("threadA");
+
+        Thread threadB = new Thread(userB);
+        threadB.setName("threadB");
+
+        threadA.start();
+//        sleep(2000); // A가 다 끝날 때까지 기다리므로 동시성 문제 X
+        sleep(100); // A가 다 끝날 때까지 기다리지 못하므로 동시성 문제 O
+        threadB.start();
+
+        sleep(3000);
+        log.info("main exit");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
