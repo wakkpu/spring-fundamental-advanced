ba9f1701c4b6e5a725578dbe80f1ebb118e8c87b
wakkpu <ho0524870@gmail.com>
2024-03-21 22:51:49 +0900

    전략 패턴

---
+++ src/test/java/hello/advanced/trace/strategy/code/ContextV1.java
@@ -0,0 +1,20 @@
+package hello.advanced.trace.strategy.code;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class ContextV1 {
+    private final Strategy strategy;
+
+    public void execute() {
+        long startTime = System.currentTimeMillis();
+
+        strategy.call();
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+}
