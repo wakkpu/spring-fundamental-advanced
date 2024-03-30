64fc3e2b66cec5f4fa4c3c52c729982876518719
wakkpu <ho0524870@gmail.com>
2024-03-30 16:45:17 +0900

    [INIT] 신규 프로젝트 생성

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
 	}
