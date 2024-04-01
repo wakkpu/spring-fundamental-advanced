e925875113244b6f24af44ab819af54d96cd0cff
전종민 <ho05248@naver.com>
2024-04-01 22:53:10 +0900

    [FEAT] 예제 프로젝트 v2 - 구현 클래스 - 스프링 빈 수동 등록

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,5 +1,6 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.Import;
  * "`AppConfig.class`를 스프링 빈으로 등록한다"
  * 단, hello.proxy.app 이하의 경로의 패키지에 대해서만 component scan의 대상이 된다
  */
-@Import(AppV2Config.class)
+@Import({AppV1Config.class, AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 	public static void main(String[] args) {
