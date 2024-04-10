d03c4cdf88253583f170d36b53982c41f17f7c01
전종민 <ho05248@naver.com>
2024-04-10 23:39:06 +0900

    [FEAT] 데코레이터 패턴 데코레이터 패턴을 통해 부가 기능 추가

---
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -0,0 +1,29 @@
+package hello.proxy.pureproxy.decorator;
+
+import hello.proxy.pureproxy.decorator.code.Component;
+import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.proxy.pureproxy.decorator.code.MessageDecorator;
+import hello.proxy.pureproxy.decorator.code.RealComponent;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class DecoratorPatternTest {
+    @Test
+    void noDecoratorTest() {
+        Component realComponent = new RealComponent();
+        DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+
+        client.execute();
+    }
+
+    @Test
+    void decoratorTest1() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+
+        client.execute();
+    }
+}
