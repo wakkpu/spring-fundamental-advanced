118e68c2182676819a51dcccfe750e22d3c49938
전종민 <ho05248@naver.com>
2024-04-20 00:03:47 +0900

    [FEAT] JDK 동적 프록시

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,6 +2,7 @@ package hello.proxy;
 
 import hello.proxy.config.proxy.v1.concreteproxy.ConcreteProxyConfig;
 import hello.proxy.config.proxy.v1.interfaceproxy.InterfaceProxyConfig;
+import hello.proxy.config.proxy.v2.handler.DynamicProxyBasicConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -15,7 +16,8 @@ import org.springframework.context.annotation.Import;
  */
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class)
+//@Import(ConcreteProxyConfig.class)
+@Import(DynamicProxyBasicConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 	public static void main(String[] args) {
