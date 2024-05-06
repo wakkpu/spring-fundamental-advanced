175ceee61f50952c31ca645d38515cd7d8bde7b6
wakkpu <ho0524870@gmail.com>
2024-05-06 16:31:26 +0900

    [FEAT] pointcut signature - type name pattern

--- aop/src/main/java/hello/aop/order/aspect/AspectV2.java
+++ aop/src/main/java/hello/aop/order/aspect/AspectV2.java
@@ -12,9 +12,27 @@ public class AspectV2 {
     @Pointcut("execution(* hello.aop.order..*(..))")
     private void allOrder() {} // pointcut signature
 
+    @Pointcut("execution(* *..*Service.*(..))") // 클래스 이름 패턴이 *Service
+    private void allService() {}
+
     @Around("allOrder()")
     public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
         log.info("[log] {}", joinPoint.getSignature());
         return joinPoint.proceed();
+    }
+
+    @Around("allOrder() && allService()") // hello.aop.order패키지와 하위 패키지이면서 클래스 이름 패턴이 *Service
+    public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
+        try {
+            log.info("[begin transaction] {}", joinPoint.getSignature());
+            Object result = joinPoint.proceed();
+            log.info("[commit transaction] {}", joinPoint.getSignature());
+            return result;
+        } catch (Exception e) {
+            log.info("[rollback transaction] {}", joinPoint.getSignature());
+            throw e;
+        } finally {
+            log.info("[resource release] {}", joinPoint.getSignature());
+        }
     }
 }
